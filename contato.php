6ca7791ba856bfa3a5404eaa44ecfab072a9d22f
dayana <dayana.mendes@etec.sp.gov.br>
2023-10-30 14:27:08 -0300

    puss in boots

---
+++ contato.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="sobrenos.php">Sobre Nós</a></li>
+                <li><a href="contato.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>SHREK</h1>
+        <p>Shrek é um ogro que vivia feliz e sozinho num pântano, em meio à floresta, em uma terra chamada Duloc. Repentinamente, ele vê sua solidão ameaçada quando o 
+        governante de Duloc, Lord Farquaad, decide expulsar todas as criaturas mágicas para floresta. Shrek fica muito irritado e oferece um acordo com Lord Farquaad: 
+        ele iria buscar a mulher dos sonhos de Lord Farquaad, a princesa Fiona, que vivia adormecida, aprisionada num castelo guardada por um dragão, e Farquaad tiraria
+        todas as criaturas mágicas da floresta, devolvendo o sossego de seu pântano. Shrek parte em sua missão, acompanhado por um burro falante, que se une a ele por 
+        gratidão, pois Shrek teria salvo a sua vida. Os dois conseguem libertar a princesa mas, no caminho de volta a Duloc, quando os dois começam a se conhecer melhor, 
+        acabam por se apaixonarem um pelo outro, e Shrek tenta impedi-la de se casar com Lord Farquaad. No final da disputa entre os dois, Shrek vence, se casa com a 
+        princesa.</p>
+    </main>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sherek</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="sobrenos.php">Sobre Nós</a></li>
+                <li><a href="contato.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>GATO DE BOTAS</h1>
+        <p></p>
+    </main>
+</body>
+</html>
