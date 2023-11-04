4fce32c4020f7f2fdcf2a584748076998c83b083
dayana <dayana.mendes@etec.sp.gov.br>
2023-11-04 17:23:45 -0300

    home

--- home.php
+++ home.php
@@ -3,17 +3,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sherek</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
 </head>
 <body>
     <header>
-        <nav>
+        <h1>NOME DA EMPRESA</h1>
+        <nav class="nav">
             <ul>
-                <li><a href="home.php">Home</a></li>
-                <li><a href="sobrenos.php">Sobre Nós</a></li>
-                <li><a href="contato.php">Contato</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre Nós</a></li>
+                <li><a href="#">Contato</a></li>
             </ul>
         </nav>
     </header>
+    <div class="container">
+        <div class="imagens-emp">
+            <div class="img1">
+                <img src="imagens/group-people-working-out-business-plan-office.jpg" alt="" width="80%">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia autem tenetur iste explicabo nemo placeat aperiam deserunt amet exercitationem nesciunt harum repudiandae, tempora cumque voluptatum? Laudantium officia optio ipsam eius.</p>
+            </div>
+            <div class="img2">
+                <img src="imagens/baixo-angulo-de-arranha-ceus.jpg" alt="" width="80%">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis delectus dolore voluptatum quas fugit quam iusto, in dicta numquam quo molestiae quidem corrupti accusamus incidunt, magnam exercitationem. Asperiores, voluptates sed.</p>
+            </div>
+            <div class="img3">
+                <img src="imagens/grupo-de-pessoas-trabalhando-no-plano-de-negocios-em-um-escritorio.jpg" alt="" width="80%">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quae deserunt adipisci voluptatem laboriosam similique repudiandae, molestias nobis aliquam alias obcaecati culpa minima temporibus dicta unde a modi animi libero.</p>
+            </div>
+        </div>
+    </div>
+    <div class="info-emp">
+        <div class="imagem-dono">
+            <img src="imagens/dono da empresa.png" alt="" width="50%">
+        </div>
+        <div class="desc-dono">
+            <h2>Dono da Empresa</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis quos error debitis dicta, temporibus consequuntur facilis ipsum at eius neque illum voluptas nostrum commodi quas accusantium cupiditate. Quisquam, adipisci maxime!</p>
+        </div>
+    </div>
+    <div class="redes-sociais">
+        <div class="rede-social-emp">
+            <div class="rede1">
+                <img src="imagens/instagram (1).png" alt="">
+                <div class="rede-name">
+                    <a href="">Instagram</a>
+                </div>
+            </div>
+            <div class="rede2">
+                <img src="imagens/linkedin (2).png" alt="">
+                <div class="rede-name">
+                    <a href="">Linkedin</a>
+                </div>
+            </div>
+            <div class="rede3">
+                <img src="imagens/facebook (2).png" alt="">
+                <div class="rede-name">
+                    <a href="">Facebook</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="rodape">
+            <span>&copy;Programação para Internet, 2023</span>
+        </div>
+    </footer>
 </body>
 </html>
