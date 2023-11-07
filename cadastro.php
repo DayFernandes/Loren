98731cb17f31a3dc0f63f3e0554f5ed505b8a510
Vitor-Amaral-l <142839726+Vitor-Amaral-l@users.noreply.github.com>
2023-11-07 13:23:20 -0300

    Atualizar o cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -19,12 +19,16 @@
     </header>
     <form method="POST" action="controler.php">
         <label>Digite seu CPF</label>
-        <p><input type="text" name="cpfaluno" placeholder="CPF ALUNO"></p>
+        <p><input type="text" name="cpf_usu" placeholder="CPF"></p>
         <p><label>Digite seu Nome</label></p>
-        <p><input type="text" name="nomealuno" placeholder="NOME ALUNO"></p>
-        <p><label>Digite sua Turma</label></p>
-        <p><input type="text" name="turmaaluno" placeholder="TURMA ALUNO"></p> 
-        <p><input type="submit" name="botao" value="CADASTRAR"></p><br>
+        <p><input type="text" name="nome_usu" placeholder="NOME ALUNO"></p>
+        <p><label>Digite seu email</label></p>
+        <p><input type="email" name="email_usu" placeholder="EMAIL"></p>
+  <p><label>Defina uma senha</label></p> 
+         <p><input type="passoword" name="senha_usu" placeholder="SENHA"></p>
+<p><label>Escreva uma mensagem</label></p> 
+         <p><input type="text" name="men_usu" placeholder="MENSAGEM..."></p>
+<p><input type="submit" name="botao" value="CADASTRAR"></p><br>
         <p><input type="submit" name="botao" value="ATUALIZAR"></p><br>
         <p><input type="submit" name="botao" value="EXCLUIR"></p><br>
         <p><input type="submit"name="botao" value="CONSULTAR"></p><br>
